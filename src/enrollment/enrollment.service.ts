f520e9e7f840558e1cc1f325cabf5b3cc7da7a01
Kevin Langga <kikoirfan45@gmail.com>
2026-09-12 11:14:58 +0700

    pengujian end point untuk enrollment dan my courses-201 ok

--- src/enrollment/enrollment.service.ts
+++ src/enrollment/enrollment.service.ts
@@ -2,9 +2,11 @@ import {
   Injectable,
   ConflictException,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateEnrollmentDto } from './dto/create-enrollment.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class EnrollmentService {
@@ -12,6 +14,18 @@ export class EnrollmentService {
 
   async enroll(studentId: string, createEnrollmentDto: CreateEnrollmentDto) {
     const { courseId } = createEnrollmentDto;
+
+    // Validasi user dari token masih ada di database
+    const student = await this.prisma.user.findUnique({
+      where: { id: studentId },
+      select: { id: true },
+    });
+
+    if (!student) {
+      throw new BadRequestException(
+        'User siswa tidak valid atau sudah tidak tersedia',
+      );
+    }
 
     // 1. Cek apakah course ada
     const course = await this.prisma.course.findUnique({
@@ -37,27 +51,41 @@ export class EnrollmentService {
     }
 
     // 3. Simpan pendaftaran baru
-    return this.prisma.enrollment.create({
-      data: {
-        studentId,
-        courseId,
-      },
-      include: {
-        course: {
-          select: {
-            id: true,
-            title: true,
-            description: true,
-            instructor: {
-              select: {
-                name: true,
-                email: true,
+    try {
+      return await this.prisma.enrollment.create({
+        data: {
+          studentId,
+          courseId,
+        },
+        include: {
+          course: {
+            select: {
+              id: true,
+              title: true,
+              description: true,
+              instructor: {
+                select: {
+                  name: true,
+                  email: true,
+                },
               },
             },
           },
         },
-      },
-    });
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException('Anda sudah mendaftar di course ini');
+        }
+
+        if (error.code === 'P2003') {
+          throw new BadRequestException('Data enrollment tidak valid');
+        }
+      }
+
+      throw error;
+    }
   }
 
   // Mengambil daftar course yang diikuti oleh siswa
